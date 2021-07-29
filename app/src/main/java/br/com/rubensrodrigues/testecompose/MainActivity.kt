e331e4172ab1fa57e974ea8e1aa0f202cb93fa47
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2021-07-29 18:24:41 -0300

    feat: (Finished first class)

--- app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
@@ -1,43 +1,32 @@
 package br.com.rubensrodrigues.testecompose
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import br.com.rubensrodrigues.testecompose.R
-import br.com.rubensrodrigues.testecompose.ui.theme.TesteComposeTheme
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+
         }
     }
 }
 
-@Composable
 @Preview
-fun MyApp() {
-    MaterialTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Teste Rubens")},
-                )
-            },
-            content = {
-                Text(
-                    text = "Bonde do Compose loco!"
-                )
-            }
-        )
-    }
+@Composable
+fun Preview() {
+    Greeting(name = "Johnson")
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "E ai, $name?")
 }
