2ba76d2d3b42600a3b97c73b30a9bf5fb568a639
Rubens <rubens.rodrigues@mobile2you.com.br>
2021-07-30 09:36:59 -0300

    feat: (Centered image)

--- app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -42,15 +44,18 @@ fun NewsStory() {
                     modifier = Modifier
                         .height(180.dp)
                         .fillMaxHeight()
-                        .clip(shape = RoundedCornerShape(4.dp)),
+                        .clip(shape = RoundedCornerShape(4.dp))
+                        .align(Alignment.CenterHorizontally),
                     contentScale = ContentScale.Crop
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Text(
-                    text = "Um belo dia ensolarado perto do arco no centro de Osasco",
-                    style = typography.h6
+                    text = "Um belo dia ensolarado perto do arco no centro de Osasco, lar do melhor cachorro quente do mundo",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     text = "Osasco, SP",
