463682c3e555c53bd9ec1605000c8dc8504a273c
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2021-07-29 23:31:23 -0300

    feat: (Advanced third class)

--- app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/testecompose/MainActivity.kt
@@ -3,12 +3,20 @@ package br.com.rubensrodrigues.testecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +28,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
-fun Preview() {
-    Greeting(name = "Johnson")
+fun NewsStory() {
+    MaterialTheme {
+//        Scaffold {
+        val typography = MaterialTheme.typography
+        Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(180.dp)
+                        .fillMaxHeight()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Um belo dia ensolarado perto do arco no centro de Osasco",
+                    style = typography.h6
+                )
+                Text(
+                    text = "Osasco, SP",
+                    style = typography.body2
+                )
+                Text(
+                    text = "18 de janeiro",
+                    style = typography.body2
+                )
+            }
+//        }
+    }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "E ai, $name?")
+fun DefaultPreview() {
+    NewsStory()
 }
