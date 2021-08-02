6e4ec1819bfccc8e972ebcbd18c9f39160f27a37
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2021-08-01 22:44:42 -0300

    feat: (Made a Constraint Layout screen)

--- app/src/main/java/br/com/rubensrodrigues/testecompose/TesteConstraint.kt
+++ app/src/main/java/br/com/rubensrodrigues/testecompose/TesteConstraint.kt
@@ -1,2 +1,43 @@
 package br.com.rubensrodrigues.testecompose
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Preview(showSystemUi = true)
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button, text) = createRefs()
+
+        Button(
+            onClick = {},
+            modifier = Modifier.constrainAs(button) {
+                top.linkTo(
+                    anchor = parent.top,
+                    margin = 16.dp
+                )
+
+                centerHorizontallyTo(parent)
+            }
+        ) {
+            Text(text = "Button")
+        }
+
+        Text(
+            text = "Text",
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(
+                    anchor = button.bottom,
+                    margin = 16.dp
+                )
+
+                centerHorizontallyTo(button)
+            }
+        )
+    }
+}
